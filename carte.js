b325c08b0c53c8d803228fab1c6caaca6ba0cb2f
leajeantet <119804942+leajeantet@users.noreply.github.com>
2023-03-15 11:52:17 +0100

    nouveaux html et js

--- carte.js
+++ carte.js
@@ -1,7 +1,5 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+const mymap = L.map('mapid').setView([48.856614, 2.3522219], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+  maxZoom: 18
 }).addTo(mymap);
-L.marker([51.5, -0.09]).addTo(mymap)
-	.bindPopup("<b>Hello world!</b><br />Je suis une popup.").openPopup();
